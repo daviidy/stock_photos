e17347db24ded12431ece72f6b560118270e2342
daviidy <yaodavidarmel@gmail.com>
2021-04-07 16:17:03 +0000

    added style for photodetails component

--- src/components/PhotoDetails.js
+++ src/components/PhotoDetails.js
@@ -1,9 +1,27 @@
 import PropTypes from 'prop-types';
 
 const PhotoDetails = ({ photo }) => (
-  <div>
+  <div className="shadow p-3 rounded">
+    <div className="row">
+      <div className="col-2">
+        <i className="fas fa-user" />
+      </div>
+      <div className="col-10">
+        <p>
+          By
+          {' '}
+          {photo.user.name}
+        </p>
+      </div>
+    </div>
+    <div className="row">
+      <p className="col-12">
+        <strong>Description: </strong>
+        {photo.description}
+      </p>
+    </div>
     <p>
-      Put the photo details here:
+
       {photo.alt_description}
     </p>
   </div>
@@ -15,6 +33,10 @@ PhotoDetails.propTypes = {
   photo: PropTypes.shape({
     id: PropTypes.string,
     alt_description: PropTypes.string,
+    description: PropTypes.string,
+    user: PropTypes.shape({
+      name: PropTypes.string,
+    }),
     urls: PropTypes.shape({
       regular: PropTypes.string,
     }),
